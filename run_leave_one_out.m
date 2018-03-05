eda5ed13624fe798bc4d779d473b312308b45cde
Laura Moraes <laura.moraes@twistsystems.com>
2018-03-05 18:04:55 -0300

    Separating LOO run

---
+++ run_leave_one_out.m
@@ -0,0 +1,6 @@
+p = genpath('.');
+addpath(p);
+original_dataset_path = '../data/seb_cell_2015_07_23_norm_lpl+mzl_no_extras.mat';
+load(original_dataset_path);
+marc = 1:max(size(marc_label));
+borders_leave_one_out(original_dataset_path,marc,marc_label);
