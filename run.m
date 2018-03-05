2a882841d4e333dc320cf999cb5fbda4d86528c0
Laura Moraes <laura.moraes@twistsystems.com>
2018-03-05 17:31:13 -0300

    Adding all files to path

--- run.m
+++ run.m
@@ -1,4 +1,6 @@
-original_dataset_path = 'Datasets/Dataset/seb_cell_2015_07_23_norm_lpl+mzl_no_extras.mat';
+p = genpath('.');
+addpath(p);
+original_dataset_path = '../data/seb_cell_2015_07_23_norm_lpl+mzl_no_extras.mat';
 load(original_dataset_path);
 marc = 1:max(size(marc_label));
 borders_leave_one_out(original_dataset_path,marc,marc_label);
